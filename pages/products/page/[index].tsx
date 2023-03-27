8cf26418f25a72ba16fc91c567d22af236340b07
Patryk Keska <PatrykKeska4@gmail.com>
2023-03-27 22:56:31 +0200

    small hotfixes plus styling issues fixed

--- pages/products/page/[index].tsx
+++ pages/products/page/[index].tsx
@@ -1,3 +1,4 @@
+import { Layout } from "@/components/Layout";
 import { PaginationSSG } from "@/components/PaginationSSG";
 import { ProductListItem } from "@/components/ProductListItem";
 import { InferGetStaticPropsType } from "next";
