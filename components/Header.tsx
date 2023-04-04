92203c052d7185b0baa510d188c500d475f50020
Patryk Keska <PatrykKeska4@gmail.com>
2023-04-04 08:14:44 +0200

    Added cart Icons(Link) to the navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,29 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { CartBar } from "./Cart/CartBar";
 
 export const Header = () => {
   const router = useRouter();
   return (
-    <nav className="bg-green-200 max-w-7xl mx-auto w-full px-4 py-2 flex gap-5 ">
-      <Link className={router.asPath == "/" ? "active" : ""} href="/">
-        Główna
-      </Link>
-      <Link className={router.asPath == "/about" ? "active" : ""} href="/about">
-        About
-      </Link>
-      <Link
-        className={router.asPath == "/about" ? "active" : ""}
-        href="/products/page/1"
-      >
-        Products
-      </Link>
+    <nav className="bg-gray-700 max-w-7xl mx-auto w-full px-4 py-2 flex gap-5 items-center justify-between ">
+      <ul className="px-4 py-2 flex gap-5 text-white">
+        <Link className={router.asPath == "/" ? "active" : ""} href="/">
+          Główna
+        </Link>
+        <Link
+          className={router.asPath == "/about" ? "active" : ""}
+          href="/about"
+        >
+          About
+        </Link>
+        <Link
+          className={router.asPath == "/about" ? "active" : ""}
+          href="/products/page/1"
+        >
+          Products
+        </Link>
+      </ul>
+      <CartBar />
     </nav>
   );
 };
