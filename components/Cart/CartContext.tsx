6a2df39c7ab361574d056b7aebb604d1cdbbcf43
Patryk Keska <PatrykKeska4@gmail.com>
2023-04-06 08:26:08 +0200

    Added Logic for Cart: - Adding products to cart, extended by counting them - Removing products from basket

--- components/Cart/CartContext.tsx
+++ components/Cart/CartContext.tsx
@@ -1,13 +1,17 @@
 import { ReactNode, createContext, useContext, useState } from "react";
 
 interface CartItem {
-  price: number;
-  title: string;
+  readonly price: number;
+  readonly title: string;
+  readonly count: number;
+  readonly id: string;
 }
 
 interface CartState {
   items: CartItem[];
+
   addItemToCart: (item: CartItem) => void;
+  removeItemFromCart: (id: CartItem["id"]) => void;
 }
 
 export const cartStateContext = createContext<CartState | null>(null);
@@ -20,7 +24,40 @@ export const CartStateProvider = ({ children }: { children: ReactNode }) => {
       value={{
         items: cartItems,
         addItemToCart: (item) => {
-          setCartItems((prevState) => [...prevState, item]);
+          setCartItems((prevState) => {
+            const exisitingItem = prevState.find(
+              (existingItem) => existingItem.id === item.id
+            );
+            if (!exisitingItem) {
+              return [...prevState, item];
+            }
+            return prevState.map((exisitingItem) => {
+              if (exisitingItem.id === item.id) {
+                return {
+                  ...exisitingItem,
+                  count: exisitingItem.count + 1,
+                };
+              }
+              return exisitingItem;
+            });
+          });
+        },
+        removeItemFromCart: (id) => {
+          setCartItems((prevState) => {
+            const exisitingItem = prevState.find((item) => item.id === id);
+            if (exisitingItem && exisitingItem?.count <= 1) {
+              return prevState.filter((item) => item.id !== id);
+            }
+            return prevState.map((item) => {
+              if (item.id === id) {
+                return {
+                  ...item,
+                  count: item.count - 1,
+                };
+              }
+              return item;
+            });
+          });
         },
       }}
     >
