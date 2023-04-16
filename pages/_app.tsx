1ce5021ad732a9a0284bef5ca49d243a3fb982da
Patryk Keska <PatrykKeska4@gmail.com>
2023-04-16 12:37:24 +0200

    Added Apollo CLient configuraion Added Apollo Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,20 @@ import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import SEO from "../next-seo.config";
 import { CartStateProvider } from "@/components/Cart/CartContext";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "@/graphql/apolloClient";
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
-    <CartStateProvider>
-      <Layout>
-        <DefaultSeo {...SEO} />
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </Layout>
-    </CartStateProvider>
+    <ApolloProvider client={apolloClient}>
+      <CartStateProvider>
+        <Layout>
+          <DefaultSeo {...SEO} />
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </Layout>
+      </CartStateProvider>
+    </ApolloProvider>
   );
 }
