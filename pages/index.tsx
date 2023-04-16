38595cda4d72c45837078f4748de7833b4832b36
Patryk Keska <PatrykKeska4@gmail.com>
2023-04-16 12:36:54 +0200

    changed in dummy home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,5 @@
-import { Layout } from "@/components/Layout";
-import { Main } from "@/components/Main";
-
-const dummyData = {
-  text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quasi porro distinctio veniam voluptates magnam optio iste nulla iure libero tempore aperiam tenetur earum placeat, iusto ratione! Totam, ducimus fugit!",
-  img: "https://picsum.photos/id/1060/536/354",
+const HomePage = () => {
+  return <div>this is home page</div>;
 };
 
-const Home = () => {
-  return (
-    <>
-      <Main>This is main page</Main>
-    </>
-  );
-};
-
-export default Home;
+export default HomePage;
