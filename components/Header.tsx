cfcb63bb63fdc0245545b2934a9b0128dba2457b
Patryk Keska <PatrykKeska4@gmail.com>
2023-04-16 12:32:34 +0200

    Navigation routing changed

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,20 +5,20 @@ import { CartBar } from "./Cart/CartBar";
 export const Header = () => {
   const router = useRouter();
   return (
-    <nav className="bg-gray-700 max-w-7xl mx-auto w-full px-4 py-2 flex gap-5 items-center justify-between ">
-      <ul className="px-4 py-2 flex gap-5 text-white">
-        <Link className={router.asPath == "/" ? "active" : ""} href="/">
+    <nav className='bg-gray-700 max-w-7xl mx-auto w-full px-4 py-2 flex gap-5 items-center justify-between '>
+      <ul className='px-4 py-2 flex gap-5 text-white'>
+        <Link className={router.asPath == "/" ? "active" : ""} href='/'>
           Główna
         </Link>
         <Link
           className={router.asPath == "/about" ? "active" : ""}
-          href="/about"
+          href='/about'
         >
           About
         </Link>
         <Link
           className={router.asPath == "/about" ? "active" : ""}
-          href="/products/page/1"
+          href='/products/'
         >
           Products
         </Link>
