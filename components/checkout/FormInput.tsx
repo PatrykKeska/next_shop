bf633673ac5a2eff61e8040e1b2aa9737b747b66
Patryk Keska <PatrykKeska4@gmail.com>
2023-05-07 18:59:11 +0200

    prepared new component to handle diffrent type of Input in one place

--- components/checkout/FormInput.tsx
+++ components/checkout/FormInput.tsx
@@ -1,36 +1,27 @@
-import {creditCardValidate} from "@/utils/creditCardValidate";
+import { creditCardValidate } from "@/utils/creditCardValidate";
 import * as React from "react";
-import {FormData} from "@/pages/checkout";
-import { FormState, UseFormRegister} from "react-hook-form";
-
-
+import { FormData } from "@/pages/checkout";
+import { FormState, UseFormRegister } from "react-hook-form";
 
 interface FormInputProps {
-useForm: {
+  useForm: {
     register: UseFormRegister<FormData>;
     formState: FormState<FormData>;
+  };
 }
-}
 
+export const FormInput = ({ useForm }: FormInputProps) => {
+  const { register, formState } = useForm;
 
-
-export const FormInput = ({useForm}: FormInputProps) => {
-    const {register,formState} = useForm
-
-
-
-
-
-    return (
-        <>
-            <input {...register("expirationDate",
+  return (
+    <>
+      {/* <input {...register("expirationDate",
                 {
                     required: true,
                     validate: creditCardValidate,
                 })}
             />
-            <span>{formState.errors.expirationDate?.message}</span>
-
-        </>
-    )
-}
+            <span>{formState.errors.expirationDate?.message}</span> */}
+    </>
+  );
+};
