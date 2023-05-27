3a93245e112d1b34875d5cc862c95a236e2123f7
Patryk Keska <PatrykKeska4@gmail.com>
2023-05-27 12:42:48 +0200

    added Newsletter cart component

---
+++ components/NewsCart.tsx
@@ -0,0 +1,20 @@
+export const NewsCart = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-extrabold sm:text-5xl'>
+        Exclusive Offers Await!
+        <strong className='font-extrabold text-red-700 sm:block'>
+          Join Our Newsletter Today.
+        </strong>
+      </h1>
+
+      <p className='mt-4 sm:text-xl/relaxed'>
+        Stay in the loop and unlock a world of exclusive offers, product
+        updates, and insider perks. Sign up for our newsletter and be the first
+        to know about new arrivals, limited-time promotions, and exciting sales
+        events. Don't miss out on the latest trends and savings – join our
+        community today!
+      </p>
+    </>
+  );
+};
