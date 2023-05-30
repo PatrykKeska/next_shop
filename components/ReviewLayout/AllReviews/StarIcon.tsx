342bc15f0210356c1c44c2a320ffb7c8791e5a6a
Patryk Keska <PatrykKeska4@gmail.com>
2023-05-30 20:44:07 +0200

    - Changed StarIcon component to be able to render half of the star for decimal numbers - Review form got slug from props insted of taking it from router slug - temporary created optimistic update and re fetching for reviews adding - changed Mutation CreateProductReview

--- components/ReviewLayout/AllReviews/StarIcon.tsx
+++ components/ReviewLayout/AllReviews/StarIcon.tsx
@@ -1,28 +1,101 @@
-import clsx from "clsx";
-
 interface StarIconProps {
   value: number;
 }
 export const StarIcon = ({ value }: StarIconProps) => {
+  const starsToRender = renderStars(value);
+  return <div className='flex'>{starsToRender}</div>;
+};
+
+const FullStarIcon = ({ value }: { value: number }) => {
   return (
-    <div className='flex'>
-      {[1, 2, 3, 4, 5].map((starValue) => (
-        <svg
-          key={starValue}
-          xmlns='http://www.w3.org/2000/svg'
-          className={clsx(
-            ` h-5 w-5 ${
-              starValue <= Math.round(value)
-                ? "text-yellow-400"
-                : "text-gray-300"
-            } cursor-pointer`
-          )}
-          viewBox='0 0 20 20'
-          fill='currentColor'
-        >
-          <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-        </svg>
-      ))}
-    </div>
+    <svg
+      key={value}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      className='w-6 h-6'
+    >
+      <path
+        d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+        fill='rgb(250 204 21)'
+      />
+      <path
+        d='M12 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+        fill='rgb(250 204 21)'
+        clip-path='url(#halfStarClip)'
+      />
+      <clipPath id='halfStarClip'>
+        <rect x='0' y='0' width='12' height='24' />
+      </clipPath>
+    </svg>
   );
+};
+
+const HalfStarIcon = ({ value }: { value: number }) => {
+  return (
+    <svg
+      key={value}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      className='w-6 h-6'
+    >
+      <path
+        d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+        fill='rgb(156 163 175)'
+      />
+      <path
+        d='M12 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+        fill='rgb(250 204 21)'
+        clip-path='url(#halfStarClip)'
+      />
+      <clipPath id='halfStarClip'>
+        <rect x='0' y='0' width='12' height='24' />
+      </clipPath>
+    </svg>
+  );
+};
+
+const GrayStarIcon = ({ value }: { value: number }) => {
+  return (
+    <svg
+      key={value}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      className='w-6 h-6'
+    >
+      <path
+        d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+        fill='rgb(156 163 175)'
+      />
+      <path
+        d='M12 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+        fill='rgb(156 163 175)'
+        clip-path='url(#halfStarClip)'
+      />
+      <clipPath id='halfStarClip'>
+        <rect x='0' y='0' width='12' height='24' />
+      </clipPath>
+    </svg>
+  );
+};
+
+const renderStars = (value: number) => {
+  const stars = [];
+  const fullStars = Math.floor(value);
+  for (let i = 0; i < fullStars; i++) {
+    stars.push(<FullStarIcon value={i} />);
+  }
+
+  const decimal = value - Math.floor(value);
+  if (decimal >= 0.5) {
+    stars.push(<HalfStarIcon value={fullStars} />);
+  }
+
+  const emptyStars = 5 - stars.length;
+  for (let i = 0; i < emptyStars; i++) {
+    stars.push(<GrayStarIcon value={value} />);
+  }
+  return stars;
 };
