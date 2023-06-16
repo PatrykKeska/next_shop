c14b375a73ae15a353a324b7cac9b6763f66d8b4
Patryk Keska <PatrykKeska4@gmail.com>
2023-06-16 07:04:59 +0200

    Signup Api validation upgrade

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,5 +1,5 @@
 import { NextApiHandler } from "next";
-import { validateSignupMiddleware } from "./middleware/validateSignupMiddleware";
+import { validateSchemaMiddleware } from "./middleware/validateSchemaMiddleware";
 import {
   SignupFormData,
   SignupSchema,
@@ -10,32 +10,60 @@ import {
   CreateAccountDocument,
   CreateAccountMutation,
   CreateAccountMutationVariables,
+  IsExmailExistQueryVariables,
 } from "@/graphql/generated/graphql";
+import { IsExmailExistQuery } from "@/graphql/generated/graphql";
+import { IsExmailExistDocument } from "@/graphql/generated/graphql";
+
+interface error {
+  message: string;
+  code: number;
+}
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body as SignupFormData;
-    const passwordHash = await bcrypt.hash(password, 12);
-
-    const user = await authorizatedApolloClient.mutate<
-      CreateAccountMutation,
-      CreateAccountMutationVariables
+    const { data } = await authorizatedApolloClient.query<
+      IsExmailExistQuery,
+      IsExmailExistQueryVariables
     >({
-      mutation: CreateAccountDocument,
+      query: IsExmailExistDocument,
       variables: {
-        firstName,
-        lastName,
-        email,
-        password: passwordHash,
+        email: req.body.email,
       },
     });
+    const isEmailExist =
+      data?.draftAccount?.email === req.body.email ||
+      data?.publishedAccount?.email === req.body.email;
+    if (isEmailExist) {
+      const error = new Error("Email already registered") as unknown as error;
+      error.code = 409;
+      throw error;
+    } else {
+      const { firstName, lastName, email, password } =
+        req.body as SignupFormData;
+      const passwordHash = await bcrypt.hash(password, 12);
 
-    res.status(200).json({
-      userID: user.data?.createAccount?.id,
-    });
+      await authorizatedApolloClient.mutate<
+        CreateAccountMutation,
+        CreateAccountMutationVariables
+      >({
+        mutation: CreateAccountDocument,
+        variables: {
+          firstName,
+          lastName,
+          email,
+          password: passwordHash,
+        },
+      });
+
+      res.status(200).json({
+        message: "Account created",
+      });
+    }
   } catch (error) {
-    return res.status(400).json({ message: "Something went wrong" });
+    const err = error as error;
+    return res.status(400).json({ message: err.message, code: err.code });
   }
 };
 
-export default validateSignupMiddleware(SignupSchema, handler);
+export default validateSchemaMiddleware(SignupSchema, handler);
